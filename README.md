2da4f9f9b2f474f6f27285ee09b9d5d150dfbfb9
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-17 23:38:16 +0000

    day1 on basic hello world function

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # the-gopher-learning
+
+This is my Go learning journey—basically a documentary of me wrestling with Gophers and figuring things out one bug at a time! 🐹🚀
+
+## Progress
+- Day 01: the usual basic hellow world function
---
+++ go-go-go/day01/main.go
@@ -0,0 +1,18 @@
+// Go installation
+// https://go.dev/doc/install
+
+// the plain old hello world
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("The usual hello world!")
+}
+
+// cd go-go-go/day01
+// go run main.go
+// or
+// go build main.go
+// ./main
