d96346bea230d1b47ea3c42d646eb7d5c31c1d19
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-18 21:05:15 +0000

    add day 2

---
+++ go-go-go/day02/main.go
@@ -0,0 +1,27 @@
+// https://gobyexample.com/variables
+
+package main
+
+import "fmt"
+
+func main() {
+	// declare 1 or more variable with var
+	var init = "initial"
+	fmt.Println(init)
+
+	// declare multiple variables at once
+	var num1, num2 int = 1, 2
+	fmt.Println(num1, num2)
+
+	// infer type based on initialized variable
+	var boolean = true
+	fmt.Println(boolean)
+
+	// variables declared without an initialization are zero-valued
+	var empty int
+	fmt.Println(empty)
+
+	// can initialize with :=
+	shorthand := "gopher"
+	fmt.Println(shorthand)
+}
