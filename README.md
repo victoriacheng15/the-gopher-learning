04087c4f6f855ef176572f308f2a47bc82c10523
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-20 01:49:06 +0000

    add day 3 - declare variables with const

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 This is my Go learning journey—basically a documentary of me wrestling with Gophers and figuring things out one bug at a time! 🐹🚀
 
 ## Progress
-- Day 01: the usual basic hello world function
-- Day 02: declare variables with types
+
+- Day 01: Basic "Hello, World!" function
+- Day 02: Declaring variables with types using `var`
+- Day 03: Declaring variables with types using `const`
---
+++ go-go-go/day03/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// 'const' is similar to 'var' for declaring variables.
+// The difference is that variables declared with 'var' can be re-assigned,
+// while variables declared with 'const' cannot be re-assigned.
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+
+	const n = 500000000
+
+	const d = 3e20 / n
+
+	fmt.Println(d)
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+
+	const test string = "try to re-assign me"
+	// This line will cause a compilation error
+	// Cannot re-assign a value to a const
+	// uncomment the below and run 'go run main.go'
+	test = "this will fail"
+	fmt.Println(test)
+}
